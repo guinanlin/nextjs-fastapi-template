7ddfb5e105889525b18ac6e7f8169b7eac431bc6
guinan.lin <guinan.lin@foxmail.com>
2025-03-30 15:25:15 +0800

    删除未使用的导入语句，简化基础模型代码

--- fastapi_backend/app/model/base_model.py
+++ fastapi_backend/app/model/base_model.py
@@ -1,10 +1,6 @@
 from fastapi_users.db import SQLAlchemyBaseUserTableUUID
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy.dialects.postgresql import UUID
-from uuid import uuid4
-
 
 class Base(DeclarativeBase):
     pass
